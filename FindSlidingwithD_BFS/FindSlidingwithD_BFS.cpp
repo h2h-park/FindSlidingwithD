bbe3c7930358770e71f472c6a2e57065a52ecf93
Heehyun Park <heehyunbpark@gmail.com>
2020-05-21 03:22:42 -0400

    Generating points with BFS, online, less memory

---
+++ FindSlidingwithD_BFS/FindSlidingwithD_BFS.cpp
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <time.h>
+#include <Windows.h>
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+#define ll long long
+
+using namespace std;
+
+struct pts {
+	ll x1, y1, x2, y2;
+	bool operator == (const pts& rhs) {
+		return (x1 == rhs.x1) && (x2 == rhs.x2) && (y1 == rhs.y1) && (y2 == rhs.y2);
+	}
+};
+struct triangle {
+	ll D, s, a, b, c;
+	pts v;
+};
+vector<triangle> minTriangles;
+vector<triangle> previousTri;
+vector<triangle> minimalTriangles;
+ll minDoubledArea;
+
+struct dat {
+	ll x, y, D;
+	bool operator < (const dat& rhs) const {
+		return D < rhs.D;
+	}
+};
+std::priority_queue<dat> pq;
+
+class pt {
+public:
+	ll x, y;
+	friend std::hash<pt>;
+	bool operator == (const pt& rhs) const {
+		return (x == rhs.x) && (y == rhs.y);
+	}
+};
+
+// define hash function for the unordered map
+namespace std {
+	template<>
+	class hash<pt> {
+	public:
+		size_t operator()(const pt& x) const {
+			using std::hash;
+			return hash<long long>()(x.x) ^ hash<long long>()(x.y) << 1;
+		}
+	};
+}
+
+std::unordered_set<pt> st; // this is an unordered_set for visit checking in bfs
+
+void getTriangles(ll x, ll y, ll D) {
+	// Find triangle such that D = l1 <= l2 <= l3 based on the point (x, y)
+	ll sidesSquared[3];
+	pts v;
+
+	ll p = (ll)(-sqrt(3.0) * (double)y + x) / 2;
+	ll q = (ll)(sqrt(3.0) * (double)x + y) / 2;
+	// Rotate (x, y) by 60 degrees
+
+	// Examine the neighborhood of the given point
+	for (ll i = x - 3; i < x + 3; i++) {
+		for (ll j = y - 3; j < y + 3; j++) {
+			ll l1 = i * i + j * j;
+			// Examine the neighborhood of the rotated point
+			for (ll k = p - 3; k < p + 3; k++) {
+				for (ll l = q - 2; l < q + 3; l++) {
+					ll l2 = k * k + l * l;
+					ll l3 = (k - i) * (k - i) + (l - j) * (l - j);
+					v.x1 = i, v.x2 = k, v.y1 = j, v.y2 = l;
+					// v has the container that has coordinates of two points
+					sidesSquared[0] = l1;
+					sidesSquared[1] = l2;
+					sidesSquared[2] = l3;
+					sort(sidesSquared, sidesSquared + 3);
+					// If the shortest length of the triangle is not D, ignore
+					if (sidesSquared[0] != D) continue;
+
+					// Get the area of this triangle
+					ll s = abs(i * l - j * k);
+
+					// Check this area is minimal
+					if (minDoubledArea > s)
+					{
+						minimalTriangles.clear();
+						minDoubledArea = s;
+						minimalTriangles.push_back({ D, s, sidesSquared[0], sidesSquared[1], sidesSquared[2], v });
+					}
+					else if (minDoubledArea == s) {
+						minimalTriangles.push_back({ D, s, sidesSquared[0], sidesSquared[1], sidesSquared[2], v });
+					}
+				}
+			}
+		}
+	}
+}
+
+void getSliding(void) {
+	int i, sz = minTriangles.size(), previousS = 0;
+	struct sliding {
+		ll D, s;
+		pts v1, v2;
+	};
+	vector<sliding> slidings;
+	for (i = sz - 1; i > 0; i--) {
+		triangle& t1 = minTriangles[i];
+		triangle& t2 = minTriangles[i - 1];
+
+		// The triangle t1 and t2 is identical, ignore.
+		if (t1.v == t2.v) continue;
+
+		// Since the triangle t1 and t2 is not identical, we need to check they share the same point.
+		if (t1.v.x1 == t2.v.x1 && t1.v.y1 == t2.v.y1) {
+			// Ignore if this triangle is updated by the previous D
+			if (!slidings.empty() && slidings.back().s == t1.s) continue;
+			slidings.push_back({ t1.D, t1.s, t1.v, t2.v });
+			printf("D : %lld S(D) = %lld base = (%lld, %lld) points = (%lld, %lld), (%lld, %lld)\n", t1.D, t1.s, t1.v.x1, t1.v.y1, t1.v.x2, t1.v.y2, t2.v.x2, t2.v.y2);
+		}
+		if (t1.v.x2 == t2.v.x2 && t1.v.y2 == t2.v.y2) {
+			if (!slidings.empty() && slidings.back().s == t1.s) continue;
+			slidings.push_back({ t1.D, t1.s, t1.v, t2.v });
+			printf("D : %lld S(D) = %lld base = (%lld, %lld) points = (%lld, %lld), (%lld, %lld)\n", t1.D, t1.s, t1.v.x2, t1.v.y2, t1.v.x1, t1.v.y1, t2.v.x1, t2.v.y1);
+		}
+	}
+}
+
+int main(void) {
+	ll D;
+	DWORD start = GetTickCount();
+	freopen("SlidingswithD.txt", "w", stdout);
+	cin >> D;
+
+	pq.push({ D, D, D * D + D * D }); // Start with (D, D)
+	for (; !pq.empty();) { // run until the priority queue is empty
+		dat now = pq.top(); // read the item from pq
+		if (now.D < 100) break; // ignore small distance cases
+		pq.pop();
+		st.erase({ now.x, now.y });
+
+		// consider the left point of the current point
+		if (now.x > 0 && st.find({ now.x - 1, now.y }) == st.end()) {
+			pq.push({ now.x - 1, now.y, (now.x - 1) * (now.x - 1) + now.y * now.y });
+			st.insert({ now.x - 1, now.y }); // visit checking
+		}
+		// consider the the point below of the current point
+		if (now.y > 0 && st.find({ now.x, now.y - 1 }) == st.end()) {
+			pq.push({ now.x, now.y - 1, now.x * now.x + (now.y - 1) * (now.y - 1) });
+			st.insert({ now.x, now.y - 1 });
+		}
+
+		// get minimial triangles when the distance is smaller or equal to D*D
+		if (now.D <= D * D) {
+			getTriangles(now.x, now.y, now.D);
+			getTriangles(now.x, -now.y, now.D);
+		}
+
+		// at the point of change of the distance, we have to update minimal triangles of the current distance
+		if (now.D != pq.top().D) {
+			minDoubledArea = pq.top().D * pq.top().D; // Initialize minDoubledArea for the next D
+
+			if (minimalTriangles.size() == 0) continue;
+
+			//minimalTriangles has information of the minimal triangle with "D = l1 <= l2 <= l3"
+			// If the minimal doubled area is larger than the previous one, we need to replace it with the previous one.
+			if (previousTri.size() != 0 && previousTri[0].s < minimalTriangles[0].s) minimalTriangles = previousTri;
+			else previousTri = minimalTriangles;
+
+			// Now minimalTriangles has information of the minimal triangle with "D <= l1 <= l2 <= l3" because we compared and updated with the previous one
+			for (auto triangles : minimalTriangles) {
+				triangles.D = now.D;
+				//Push every minimal triangles to minTriangle
+				minTriangles.push_back(triangles);
+				//pts v = triangles.v;
+				//printf("%lld %lld %lld %lld %lld : %lld %lld %lld %lld\n", D, triangles.s, triangles.a, triangles.b, triangles.c, v.x1, v.y1, v.x2, v.y2);
+			}
+			minimalTriangles.clear();
+		}
+	}
+	getSliding();
+	minTriangles.clear();
+	printf("%dms\n", GetTickCount() - start);
+
+	fclose(stdout);
+	return 0;
+}
